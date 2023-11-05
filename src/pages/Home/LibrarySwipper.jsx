51fa1900467b94db60625ec1e27b6b1c55157b9b
isalim079 <salimreza1025@gmail.com>
2023-11-05 15:52:46 +0600

    swiper info added. Also design made

--- src/pages/Home/LibrarySwipper.jsx
+++ src/pages/Home/LibrarySwipper.jsx
@@ -1,24 +1,65 @@
 /* eslint-disable react/prop-types */
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import "swiper/css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const LibrarySwipper = ({ libraryEventAndFeature }) => {
     console.log(libraryEventAndFeature);
 
+    useEffect(() => {
+        Aos.init({
+            duration: 600,
+        });
+    }, []);
+
     return (
         <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
             spaceBetween={0}
             slidesPerView={1}
+            // navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
             onSlideChange={() => console.log("slide change")}
             onSwiper={(swiper) => console.log(swiper)}
         >
             {libraryEventAndFeature.map((libraryEvent) => (
                 <SwiperSlide key={libraryEvent._id}>
-                    <img
-                        className="md:h-[720px] md:w-full"
-                        src={libraryEvent.image}
-                        alt={libraryEvent.title}
-                    />
+                    <div>
+                        <div className="absolute bg-gradient-to-r from-black h-full w-full">
+                            <div className=" text-lightCoffeeShade flex flex-col justify-center items-start md:pl-40 h-full space-y-2">
+                            <h1 className="uppercase text-6xl font-semibold" data-aos="zoom-out">
+                                {libraryEvent.title}
+                            </h1>
+                            <div className=" border-2 border-lightCoffeeShade w-[720px]"  data-aos="fade-right"></div>
+                            <p className="pt-1" data-aos="fade-right">
+                                {libraryEvent.subtitle}
+                            </p>
+
+                            <div>
+                            <button className="px-4 py-2 bg-oliveGreenShade rounded-md"  data-aos="fade-right">
+                                More Details
+                            </button>
+                        </div>
+
+                            </div>
+                        </div>
+                        <div>
+                            <img
+                                className="md:h-[720px] md:w-full"
+                                src={libraryEvent.image}
+                                alt={libraryEvent.title}
+                            />
+                        </div>
+                       
+                    </div>
                 </SwiperSlide>
             ))}
         </Swiper>
